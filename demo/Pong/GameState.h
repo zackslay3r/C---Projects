ba278919e6290ac81c13fbb7a7fcd49c4e286270
Zackary D <zackary.direen@hotmail.com>
2017-06-05 16:56:56 +1000

    init

--- demo/Pong/GameState.h
+++ demo/Pong/GameState.h
@@ -11,19 +11,7 @@ namespace aie
 
 class GameState : public IGameState {
 
-public: 
-	GameState(PongApp *app);
-	virtual ~GameState();
 
-	virtual void update(float dt);
-	virtual void draw(aie::Renderer2D *rednerer);
-
-protected:
-	std::unique_ptr<aie::Font> m_font;
-	float m_elapsedTime;
-
-	std::unique_ptr<aie::Texture> m_tex;
-	std::unique_ptr<Player>
 
 
 
