a73c834f358f11f8601a1ba03d6fcb3602c2e00d
zackslay3r <zackary.direen@hotmail.com>
2017-06-06 19:22:11 +1000

    Inital creation of my game, nothing is linked and no states are fully done.

---
+++ PongDemo/aieBootstrap-master/project2D/GameState.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+#include "IGameState.h"
+class GameEntity;
+class Player;
+
+namespace aie
+{
+	class Font;
+}
+
+class GameState : public IGameState {
+
+
+
+
+
+};
