4bb7a6191e30a07e45723e00aee62d1b9fe28a8c
Zackary D <zackary.direen@hotmail.com>
2017-06-07 17:03:47 +1000

    changed

--- PongDemo 2/aieBootstrap-master/project2D/Application2D.h
+++ PongDemo 2/aieBootstrap-master/project2D/Application2D.h
@@ -14,18 +14,32 @@ class GameStateManager;
 class Application2D : public aie::Application {
 public:
 
+	//Constructor
 	Application2D();
+
+
+	// Destructor
 	virtual ~Application2D();
 
+	//Startup
 	virtual bool startup();
+	
+	//Shutdown
 	virtual void shutdown();
 
+	// Update via deltatime
 	virtual void update(float deltaTime);
+	
+	//draws the objects.
 	virtual void draw();
+
+
+	//Scene renderer.
+	aie::Renderer2D m_renderer;
 
 protected:
 
-	std::unique_ptr<aie::Renderer2D> m_renderer;
-	std::unique_ptr<GameStateManager> m_gameStateManager;
+	// Camera position
+	float m_cameraX, m_cameraY;
 
 };
--- PongDemo 2/aieBootstrap-master/project2D/IState.h
+++ PongDemo 2/aieBootstrap-master/project2D/IState.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <string>
+
+
+class IState
+{
+	IState(); // default constructor.
+
+	virtual ~IState(); //Default destructor
+
+	// Retrieves the name of the state.
+	virtual std::string getName() = 0;
+
+
+
+	// Enter the current state.
+	virtual void enterState() = 0;
+
+
+	// Exit the current state.
+	virtual void leaveState() = 0;
+
+
+
+	// Update the current state.
+	virtual void updateState() = 0;
+
+
+	// Execute the draw state function.
+	virtual void drawState() = 0;
+
+
+};
