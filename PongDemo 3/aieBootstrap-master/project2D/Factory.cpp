9ea15dbda5404b16215e0c31f1b7510677c4de04
Zackary D <zackary.direen@hotmail.com>
2017-06-14 16:36:16 +1000

    Factory carcass.

---
+++ PongDemo 3/aieBootstrap-master/project2D/Factory.cpp
@@ -0,0 +1,25 @@
+#include "Factory.h"
+
+
+
+Factory::Factory()
+{
+}
+
+Factory::makeBall()
+{
+ 
+}
+
+Factory::makeLeftPaddle()
+{
+}
+
+Factory::makeRightPaddle()
+{
+}
+
+
+Factory::~Factory()
+{
+}
