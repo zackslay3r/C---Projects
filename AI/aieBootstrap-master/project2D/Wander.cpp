90bde8f6920681b0856076100d03401da487947d
zackslay3r <zackary.direen@hotmail.com>
2017-08-12 18:40:19 +1000

    testing

--- AI/aieBootstrap-master/project2D/Wander.cpp
+++ AI/aieBootstrap-master/project2D/Wander.cpp
@@ -22,20 +22,49 @@ Vector2 Wander::Update(float dt)
 		// then it is multiplied by the wander distance.
 
 
+		//Vector2 circleCentre;
+		//circleCentre = ((Enemy*)mySelf)->velocity;
+		//circleCentre.normalise();
+		//circleCentre = circleCentre * wanderDistance;
+
+		//Vector2 RandOnCircle = { (float)random()  ,(float)random() };
+		//RandOnCircle.normalise();
+		//RandOnCircle = RandOnCircle * wanderRadius;
+
+		//RandOnCircle = circleCentre + RandOnCircle;
+		//Vector2 v2 = RandOnCircle;
+
+		//float speed = 50.0f;
+		///*//calculate the displacement force
+		//Vector2 displacement;
+		//displacement = { 0,-1 };
+		//displacement = displacement * wanderRadius;
+
+		//// randomly change the vector direction
+		//setAngle(&displacement, wanderAngle);
+
+		//// change the wanderAngle a little bit, so its not the same every frame.
+		//wanderAngle += (random() * wanderJitter) - (wanderJitter * 0.5);
+
+		//float speed = 100.0f;
+
+		////Vector2 v1 = mySelf->position;
+		//// This is downcasting that will tell it to act as though it is a enemy pointer rather then a Object.
+		//Vector2 v2 = circleCentre + displacement;*/
+
+		////Vector2 v3 = v2 - v1;
+
+		//v2.normalise();
+
+		//return (v2 * speed * behaviourWeight);
+
+	
 		Vector2 circleCentre;
 		circleCentre = ((Enemy*)mySelf)->velocity;
 		circleCentre.normalise();
 		circleCentre = circleCentre * wanderDistance;
 
-		Vector2 RandOnCircle = { (float)random()  ,(float)random() };
-		RandOnCircle.normalise();
-		RandOnCircle = RandOnCircle * wanderRadius;
-
-		RandOnCircle = circleCentre + RandOnCircle;
-		Vector2 v2 = RandOnCircle;
-
-		float speed = 100.0f;
-		/*//calculate the displacement force
+		//calculate the displacement force
 		Vector2 displacement;
 		displacement = { 0,-1 };
 		displacement = displacement * wanderRadius;
@@ -46,19 +75,18 @@ Vector2 Wander::Update(float dt)
 		// change the wanderAngle a little bit, so its not the same every frame.
 		wanderAngle += (random() * wanderJitter) - (wanderJitter * 0.5);
 
-		float speed = 100.0f;
+		float speed = 20.0f;
 
 		//Vector2 v1 = mySelf->position;
 		// This is downcasting that will tell it to act as though it is a enemy pointer rather then a Object.
-		Vector2 v2 = circleCentre + displacement;*/
+		Vector2 v2 = circleCentre + displacement;
 
 		//Vector2 v3 = v2 - v1;
 
 		v2.normalise();
 
-		return (v2 * speed * behaviourWeight);
+		return(v2 * speed* behaviourWeight);
 
-	
 	}
 }
 
